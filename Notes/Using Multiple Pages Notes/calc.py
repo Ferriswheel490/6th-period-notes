b9c91e24e0dba86c4661bd7487d90581bb4b8ba6
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-02-24 13:24:11 -0700

    Notes

---
+++ Notes/Using Multiple Pages Notes/calc.py
@@ -0,0 +1,15 @@
+#Fairus De La Cruz Basic calculation
+def get_num():
+    return int(input("tell me a number:\n "))
+
+def addition():
+    return get_num + get_num
+
+def subtraction():
+    return get_num + get_num
+
+def multiplication():
+    return get_num * get_num
+
+def devision():
+    return  get_num / get_num
---
+++ Notes/Using Multiple Pages Notes/main.py
@@ -0,0 +1,17 @@
+#Fairus De La Cruz, Multiple Pages Notes
+
+
+#from calc import addition as add, subtraction as sub
+
+#print(add())
+#print(sub(20,9))
+
+def get_user_info():
+    name = input("what is your name:\n ")
+    age = int(input("what is your age:\n "))
+    color = input("what is your favorite color:\n ")
+    return name, age, color
+
+name, age, color = get_user_info()
+
+print(age)
