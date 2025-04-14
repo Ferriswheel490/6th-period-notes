524f15531ca8d37d17123a2768c53fc61fef4c6b
ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-04-14 13:12:38 -0600

    e

---
+++ Notes/Class Notes/main.py
@@ -0,0 +1,10 @@
+class subject:
+    def __init__(self, content, period, teacher, room):
+        self.content = content
+        self.period = period
+        self.teacher = teacher
+        self.room = room
+
+first = subject("chemestry", 1, "Jo")
+
+print(first)
